c7971364d6f94b0eacd4ce110182360972006375
Emmanuel Akolbire <dbakolbire@gmail.com>
2024-03-11 15:42:06 +0000

    feature(Courts):Add worker model

--- src/courts/admin.py
+++ src/courts/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Court
+from .models import Court,Worker
 
 
 @admin.register(Court)
 class CourtAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
     pass
--- src/courts/models.py
+++ src/courts/models.py
@@ -16,3 +16,14 @@ class Court(models.Model):
         return f"<Court:{self.court_id} | {self.name}"
 
 
+class Worker(models.Model):
+    """ Worker models """
+
+    worker_id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    name = models.CharField()
+    email = models.EmailField()
+    court = models.ForeignKey(Court, on_delete=models.CASCADE, related_name="workers")
+
+
+    def __str__(self):
+        return f"<Worker:{self.worker_id} | {self.name}"
